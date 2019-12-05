a981b675fd2e10e88511540b34966c6603fec42c
patpatpatclapclapclap <58519927+patpatpatclapclapclap@users.noreply.github.com>
2019-12-05 09:38:09 -0500

    Add files via upload

---
+++ ATM Software/src/AutoTellerMachine.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class AutoTellerMachine {
+	public static void main(String[] args){
+		
+		int scamaction;
+		Scanner userInput = new Scanner(System.in);
+		Scanner userPINput = new Scanner(System.in);
+		
+		System.out.println("Welcome to Vito Bank's Premium ATM!");
+		System.out.println("Enter a your PIN Number here to access your account");
+		
+		String userPIN = userPINput.nextLine();
+		boolean pinVerification =  false;
+		while (pinVerification = false) {
+			if (ArrayList.contains(userPIN)){
+				pinVerification = true;
+			}
+			else {
+				System.out.println("That wasn't a valid PIN the program will now run infinitley good job idiot");
+			}
+		}
+		
+	}
+	public class ATMFunctions{
+		
+		public double Withdrawal{
+			
+			/*CustomerBalance*/ = /*CustomerBalance*/ - userInput.nextDouble();
+			System.out.println("Withdrawal Complete! Your Balance is now " + /*CustomerBalance*/);
+			System.out.println("Would you like to perform another Transaction? Enter 0 for Yes, 1 for no, and 2 to end tranaction and to print your receipt");
+			scamaction = userInput.nextInt(); 
+			if (scamaction = 0) {
+				System.out.println("What sort of Transaction would you like to Perform?");
+			}
+		}
+		public double Deposit{
+			
+			/*CustomerBalance*/ = /*CustomerBalance*/ + userInput.nextDouble();
+		}
+	}
+		
+		
+
+
+}
